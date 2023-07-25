379b35b21e1fea93596ec2703af64c7e317184c1
Yan Zhimin <a13684703281@163.com>
2023-07-25 05:19:32 -0400

    Add database support with Flask-SQLAlchemy

--- Log.md
+++ Log.md
@@ -33,9 +33,124 @@ git branch -dr [remote/branch] # delete remote branch
 ## 参考资料
 
 1. [Git使用详解](https://blog.csdn.net/qq_57581439/article/details/124237905)
-
 2. [Markdown语法](https://blog.csdn.net/github_38336924/article/details/82183088)
 
 # 7.25学习记录
 
+- 昨天的补充
+  - 在运行虚拟环境前需要先确保安装了virtual
+- 环境变量管理
+    ```bash
+    export FLASK_DEBUG=1
+    flask run
+    # 或者可以使用自动导入环境变量的python-dotenv
+    # 高版本Flask可以
+    flask run --dubug
+    ```
+    `.env`文件用来存储敏感数据，不提交进Git仓库
 
+    `.flaskenv`文件将环境设为`FLASK_DEBUG`设为1,以便开启调试模式
+
+    用户输入的数据可能包含恶意代码，不能直接作为响应返回，需要使用`MarkupSafe`提供的`escape()`对变量进行转义处理
+- 数据库
+  
+   使用`SQLALchemy`--Python数据库工具，通过定义Python类来表示数据库里的一张表（类属性表示表中的字段/列），通过对类操作来代替写SQL语句，这个类称为模型类，类中属性称为字段。
+
+   ```bash
+    pip install flask-sqlalchemy==2.5.1 sqlalchemy==1.4.47
+    #  Flask-SQLAlchemy 3.x / SQLAlchemy 2.x 版本有一些大的变化，这里分别固定安装 2.5.1 和 1.4.47 版本。
+   ```
+
+   为了设置Flask/扩展/程序本身的一些行为，需要设置和定义一些配置变量。Flask提供了一个统一的接口来写入和获取这些配置变量: `Flask.config`字典。配置变量的名称必须是使用大写，写入配置的语句一般会放到扩展类实例化语句之前。
+   ```python
+    import os
+    # SQLALCHEMY_DATABASE_URI变量来告诉SQLAlchemy数据库连接地址
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+   ```
+   在Windows中是3个`/`，linux或Mac中是4个`/`
+   如果出现了`Instance of 'SQLALchemy' has no 'xxx' member`的情况，是由于`pylint`的事情，改为`flake8`，要禁用`pylint`才行，但我感觉是我没设置好的问题（在settings.json中设置）
+   如果出现了长度报错的问题，在`python-Linting`中添加相关的限定长度。
+  ```bash
+  (env) $ flask shell # 创建db文件
+  >>> from app import db
+  >>> db.create_all()
+  ```
+  - 创建
+
+  ```python
+  >>> from app import User, Movie  # 导入模型类
+  >>> user = User(name='Grey Li')  # 创建一个 User 记录
+  >>> m1 = Movie(title='Leon', year='1994')  # 创建一个 Movie 记录
+  >>> m2 = Movie(title='Mahjong', year='1996')  # 再创建一个 Movie 记录
+  >>> db.session.add(user)  # 把新创建的记录添加到数据库会话
+  >>> db.session.add(m1)
+  >>> db.session.add(m2)
+  >>> db.session.commit()  # 提交数据库会话，只需要在最后调用一次即可
+  ```
+
+  当实例化模型类时，如果没有传入主键，SQLAlchemy会自动处理该字段
+
+  - 读取
+
+    `<模型类>.query.<过滤方法(可选)>.<查询方法>`
+
+    具体的过滤方法和常用的查询方法都在参考资料2中指出。
+  
+  - 更新
+
+    ```python
+    >>> movie = Movie.query.get(2)
+    >>> movie.title = 'WALL-E'  # 直接对实例属性赋予新的值即可
+    >>> movie.year = '2008'
+    >>> db.session.commit()  # 注意仍然需要调用这一行来提交改动
+    ```
+  
+  - 删除
+
+  ```python
+  >>> movie = Movie.query.get(1)
+  >>> db.session.delete(movie)  # 使用 db.session.delete() 方法删除记录，传入模型实例
+  >>> db.session.commit()  # 提交改动
+  ```  
+
+- 在程序里操作数据库
+
+  ```python
+  @app.cli.command()
+  @click.option('--xxx', is_flag=True, help='yyy')
+  def command_name(arg):
+    """Description"""
+    if arg:
+      return 
+    else:
+      return 
+  ```
+- 模板信息
+  - vscode中 “File”—“Preferences”—“User Snippets”选择python
+  ```json 
+    {
+    "HEADER": {
+        "prefix": "header",
+        "body": [
+        "#!/usr/bin/env python",
+        "# -*- encoding: utf-8 -*-",
+        "'''",
+        "@文件    :$TM_FILENAME",
+        "@说明    :",
+        "@时间    :$CURRENT_YEAR/$CURRENT_MONTH/$CURRENT_DATE $CURRENT_HOUR:$CURRENT_MINUTE:$CURRENT_SECOND",
+        "@作者    :张三",
+        "@版本    :1.0",
+        "'''",
+        "",
+        "$0"
+        ],
+    }
+    }
+  ```
+    之后在头部敲入header即可自动弹出上面的注释。
+## 参考资料
+
+1. [Hello,Flask!](https://tutorial.helloflask.com/hello/)
+2. [数据库](https://tutorial.helloflask.com/database/)
+3. [添加Python模板信息](www.yisu.com/zixun/155844.html)
+4. [SQLAlchemy报错解决方案](https://blog.csdn.net/stone0823/article/details/90488029)
--- app.py
+++ app.py
@@ -1,46 +1,140 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@file         :app.py
+@description  :
+@time         :2023/07/24 21:40:21
+@author       :Yan ZhiMin
+@version      :1.0
+"""
+
+import os
+import sys
+import click
 from flask import Flask
 from flask import url_for
-from markupsafe import escape
 from flask import render_template
-app = Flask(__name__)
+from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
+WIN = sys.platform.startswith("win")
+prefix = ""
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的监控
+
+
+db = SQLAlchemy(app)  # 在扩展类实例化前加载配置
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # key
+    name = db.Column(db.String(20))  # users' name
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.route("/")
 def hello_world():
     return '<h1>hello world</h1><img src="/static/images/favicon.png">'
 
-@app.route('/python')
+
+@app.route("/python")
 def hello_python():
-	return 'hello python'
+    return "hello python"
 
-@app.route('/python/flask')
+
+@app.route("/python/flask")
 def hello_flask():
-	return 'hello flask'
+    return "hello flask"
 
-@app.route('/user/<name>')
+
+@app.route("/user/<name>")
 def user_page(name):
-	return f'Wlcome to {escape(name)}'
+    return f"Wlcome to {escape(name)}"
 
-@app.route('/test')
+
+@app.route("/test")
 def test_url_for():
-	print(url_for('hello_world'))
-	print(url_for('user_page', name='yzm'))
-	print(url_for('user_page', name='Alice'))
-	print(url_for('test_url_for'))
-	print(url_for('test_url_for', num=2))
-	print(url_for('static', filename='favicon.png'))
-	return 'Test Page'
+    print(url_for("hello_world"))
+    print(url_for("user_page", name="yzm"))
+    print(url_for("user_page", name="Alice"))
+    print(url_for("test_url_for"))
+    print(url_for("test_url_for", num=2))
+    print(url_for("static", filename="favicon.png"))
+    return "Test Page"
 
 
-
-name = 'Yan Zhimin'
+NAME = "Yan Zhimin"
 movies = [
-	{'title': '1', 'year': '1988'},
-	{'title': '2', 'year': '1989'},
-	{'title': '3', 'year': '1993'},
-	{'title': '4', 'year': '1982'},
-	{'title': '5', 'year': '1989'}
+    {"title": "1", "year": "1988"},
+    {"title": "2", "year": "1989"},
+    {"title": "3", "year": "1993"},
+    {"title": "4", "year": "1982"},
+    {"title": "5", "year": "1989"},
 ]
 
-@app.route('/index')
+
+@app.route("/index")
 def index():
-	return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template("index.html", user=user, movies=movies)
+
+
+@app.cli.command()  # 注册为命令，可以传入name参数来自定义命令
+@click.option("--drop", is_flag=True, help="Create after drop.")  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")
+
+@app.cli.command()
+@click.option("--yzm", is_flag=True, help="say hello to everyone.")
+def say_hello(yzm):
+    """we should be polite, so say hello to everyone!"""
+    if yzm:
+        click.echo("Hello, everyone! My name is Yan Zhimin.")
+    else:
+        click.echo("Hello~~")
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
